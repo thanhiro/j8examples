57b03c0bb06b4b4bb21311a9ca02cd2c517bd9dd
Timo Hanhirova <thanhiro@Timos-MacBook-Pro.local>
2014-06-06 11:41:19 +0400

    Add optional stuff

--- src/main/java/fi/arcusys/j8examples/resource/RootResource.java
+++ src/main/java/fi/arcusys/j8examples/resource/RootResource.java
@@ -1,11 +1,10 @@
 package fi.arcusys.j8examples.resource;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.*;
 import java.util.concurrent.atomic.LongAdder;
+
 import static fi.arcusys.j8examples.conf.Constants.*;
 
 @Path("/")
@@ -18,19 +17,26 @@ public class RootResource {
         this.adder = adder;
     }
 
+
     @GET
-    public Map<String, Object> index() {
+    // Here I'd like to have import aliasing feature
+    public Map<String, Object> index(@QueryParam("reset") com.google.common.base.Optional<String> reset) {
         adder.increment();
         String base64Str = java.util.Base64.getEncoder().encodeToString("index".getBytes());
+
+        if (reset.isPresent()) {
+            adder.reset();
+        }
 
         Map<String, Object> obj = new HashMap<>();
         obj.put("base64", base64Str);
         obj.put("counter", Long.toString(adder.sum()));
 
-        Optional<String> opt = Optional.ofNullable("MaybeEmpty");
-        opt.ifPresent(s -> obj.put("optionalValueHere", s));
+        Optional<String> opt = Optional.ofNullable("MaybeEmpty"); //Optional.empty();
+        opt.ifPresent(s -> obj.put("optionalValue", s));
 
         obj.put("urls", URLS);
         return obj;
     }
+
 }
